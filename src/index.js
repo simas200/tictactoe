2ad54752a4a688019f7c1a3ec0993054f68adf07
simas200 <siminskas@gmail.com>
2023-08-26 10:08:08 +0300

    tutorial done

--- src/index.js
+++ src/index.js
@@ -1,63 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './styles.css';
 
-class Square extends React.Component {
-    render(){
-	return (
-	    <button className="square" onCLick={function() {console.log("click!"); }}>
-		{this.props.value}
-	    </button>
-	);
-    }
-}
+import App from './App';
 
-class Board extends React.Component {
-    renderSquare(i){
-	return <Square value={i} />;
-    }
-
-    render(){
-	const status = "Next player: X";
-	
-	return(
-	    <div>
-		<div className="status">{status}</div>
-		<div classname="board-row">
-		    {this.renderSquare(0+0)}
-		    {this.renderSquare(0+1)}
-		    {this.renderSquare(0+2)}
-		</div>
-		<div classname="board-row">
-		    {this.renderSquare(3+0)}
-		    {this.renderSquare(3+1)}
-		    {this.renderSquare(3+2)}
-		</div>
-		<div classname="board-row">
-		    {this.renderSquare(6+0)}
-		    {this.renderSquare(6+1)}
-		    {this.renderSquare(6+2)}
-		</div>
-	    </div>
-	);
-    }
-}
-
-class Game extends React.Component {
-    render(){
-	return (
-	    <div className="game">
-		<div className="game-board">
-		    <Board />
-		</div>
-		<div className="game-info">
-		    <div>{/* status */}</div>
-		    <ol>{/* TODO */}</ol>
-		</div>
-	    </div>
-	);
-    }
-}
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<Game />)
+const root = createRoot(document.getElementById("root"));
+root.render(
+    <StrictMode>
+	<App />
+    </StrictMode>
+);
